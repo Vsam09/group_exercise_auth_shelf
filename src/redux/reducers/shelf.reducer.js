1e66de5faf814cdb27af991a8093cc7a8e9a178b
samson vang <vangsam2@gmail.com>
2021-09-03 11:30:12 -0500

    shelf reducer

---
+++ src/redux/reducers/shelf.reducer.js
@@ -0,0 +1,11 @@
+
+const shelfReducer = (state = [], action) => {
+    switch (action.type) {
+        case 'SET_ITEM':
+            return action.payload
+        default:
+            return state;
+    }
+        
+}
+export default shelfReducer;
